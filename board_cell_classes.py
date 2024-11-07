ea1f647417d64511cb7ad2e38bbfdb27921c8c57
Hal609 <hal@kolb.co.uk>
2024-11-07 13:31:03 +0000

    Consolidate class into one

--- board_cell_classes.py
+++ board_cell_classes.py
@@ -11,8 +11,45 @@ import torch.optim as optim
 
 from grid_vis_new import run_grid
 
-class Grid():
-    def __init__(self, size, treat_ratio = 0.7, trap_ratio = 0.2):
+# Define the Q-Network
+class DQN(nn.Module):
+    def __init__(self, n_observations, n_actions):
+        super(DQN, self).__init__()
+        self.fc1 = nn.Linear(n_observations, 128)
+        self.fc2 = nn.Linear(128, 128)
+        self.fc3 = nn.Linear(128, n_actions)  # Output is the Q-value for each action (up, down, left, right)
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        return self.fc3(x)
+
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
+
+
+class ReplayMemory(object):
+
+    def __init__(self, capacity):
+        self.memory = deque([], maxlen=capacity)
+
+    def push(self, *args):
+        """Save a transition"""
+        self.memory.append(Transition(*args))
+
+    def sample(self, batch_size):
+        return rn.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+    
+    def __str__(self):
+        frame = pd.DataFrame(self.memory, columns=['state', 'action', 'next_state', 'reward'])
+        return str(frame)
+
+
+class DLGrid():
+    def __init__(self, size, treat_ratio=0.7, trap_ratio=0.3):
         self.size = size
         self.height = size[0]
         self.width = size[1]
@@ -31,6 +68,30 @@ class Grid():
         self.init_board()
         self.initial_setup = deepcopy(self.board)
 
+        self.BATCH_SIZE = 128  # BATCH_SIZE is the number of transitions sampled from the replay buffer
+        self.GAMMA = 0.99  # GAMMA is the discount factor as mentioned in the previous section
+        self.EPS_START = 0.9  # EPS_START is the starting value of epsilon
+        self.EPS_END = 0.05  # EPS_END is the final value of epsilon
+        self.EPS_DECAY = 1000  # EPS_DECAY controls the rate of exponential decay of epsilon, higher means a slower decay
+        self.TAU = 0.005  # TAU is the update rate of the target network
+        self.LR = 1e-4  # LR is the learning rate of the ``AdamW`` optimizer
+
+        self.device = torch.device(
+                    "cuda" if torch.cuda.is_available() else
+                    "mps" if torch.backends.mps.is_available() else
+                    "cpu")
+
+        n_actions = 4
+        n_observations = self.height * self.width
+
+        self.policy_net = DQN(n_observations, n_actions).to(self.device)
+        self.target_net = DQN(n_observations, n_actions).to(self.device)
+        self.target_net.load_state_dict(self.policy_net.state_dict())
+
+        self.optimizer = optim.AdamW(self.policy_net.parameters(), lr=self.LR, amsgrad=True)
+        self.memory = ReplayMemory(10000)
+
+        self.steps_done = 0
 
     def init_board(self):
         self.board = np.zeros(self.size, dtype='U8')
@@ -65,15 +126,7 @@ class Grid():
         if 0 in pos or self.width-1 <= pos[1] or self.height-1 <= pos[0]:
             return False
         return True
-
-    def get_next(self):
-        directions = torch.tensor(((0, 1), (0, -1), (1, 0), (-1, 0)))
-        nexts = []
-        for entry in (self.agent_pos + directions):
-            if self.is_inbounds(entry):
-                nexts.append(entry)
-        return torch.stack(nexts)
-
+    
     def calc_reward(self, choice):
         new_pos = self.agent_pos + self.action_map[choice]
         next_val = self.board[new_pos[0]][new_pos[1]]
@@ -82,6 +135,16 @@ class Grid():
         elif next_val == self.colour_dict["trap"]:
             return -3
         return 0
+    
+    def next_state(self, choice):
+        next_pos = self.agent_pos + self.action_map[choice]
+        if not self.is_inbounds(next_pos): return self.board
+        next = deepcopy(self.board)
+        next[self.agent_pos[0]][self.agent_pos[1]] = "000000ff"
+        self.agent_pos = next_pos
+        next[next_pos[0]][next_pos[1]] = self.colour_dict["agent"]
+
+        return next
 
     def board_to_tensor(self, board):
         tensor_board = torch.zeros((self.board.shape))
@@ -94,98 +157,7 @@ class Grid():
         if int(torch.count_nonzero(self.board_to_tensor(self.board))) == self.num_traps + 1: return True
         if self.initial_setup[self.agent_pos[0]][self.agent_pos[1]] == self.colour_dict["trap"]: return True
         return False
-
-    def next_state(self, choice):
-        next_pos = self.agent_pos + self.action_map[choice]
-        if not self.is_inbounds(next_pos): return self.board
-        next = deepcopy(self.board)
-        next[self.agent_pos[0]][self.agent_pos[1]] = "000000ff"
-        self.agent_pos = next_pos
-        next[next_pos[0]][next_pos[1]] = self.colour_dict["agent"]
-
-        return next
-
-    def pick_action(self):
-        return rn.choice(self.get_next())
-
-    def step(self):
-        action = self.pick_action()
-        reward = self.calc_reward(action)
-        self.score += reward
-        next_state = self.next_state(action)
-        self.memory.push(self.board, action, reward, next_state, self.finished)
-        self.board = next_state
-        self.finished = self.is_done()
-
-# Define the Q-Network
-class DQN(nn.Module):
-    def __init__(self, n_observations, n_actions):
-        super(DQN, self).__init__()
-        self.fc1 = nn.Linear(n_observations, 128)
-        self.fc2 = nn.Linear(128, 128)
-        self.fc3 = nn.Linear(128, n_actions)  # Output is the Q-value for each action (up, down, left, right)
-
-    def forward(self, x):
-        x = torch.relu(self.fc1(x))
-        x = torch.relu(self.fc2(x))
-        return self.fc3(x)
-
-Transition = namedtuple('Transition',
-                        ('state', 'action', 'next_state', 'reward'))
-
-
-class ReplayMemory(object):
-
-    def __init__(self, capacity):
-        self.memory = deque([], maxlen=capacity)
-
-    def push(self, *args):
-        """Save a transition"""
-        self.memory.append(Transition(*args))
-
-    def sample(self, batch_size):
-        return rn.sample(self.memory, batch_size)
-
-    def __len__(self):
-        return len(self.memory)
     
-    def __str__(self):
-        # Create the DataFrame once with the collected rows
-        frame = pd.DataFrame(self.memory, columns=['state', 'action', 'next_state', 'reward'])
-
-        # Return the DataFrame as a string representation
-        return str(frame)
-
-
-class DLGrid(Grid):
-    def __init__(self, size, treat_ratio=0.7, trap_ratio=0.3):
-        super().__init__(size, treat_ratio, trap_ratio)
-
-        self.BATCH_SIZE = 128  # BATCH_SIZE is the number of transitions sampled from the replay buffer
-        self.GAMMA = 0.99  # GAMMA is the discount factor as mentioned in the previous section
-        self.EPS_START = 0.9  # EPS_START is the starting value of epsilon
-        self.EPS_END = 0.05  # EPS_END is the final value of epsilon
-        self.EPS_DECAY = 1000  # EPS_DECAY controls the rate of exponential decay of epsilon, higher means a slower decay
-        self.TAU = 0.005  # TAU is the update rate of the target network
-        self.LR = 1e-4  # LR is the learning rate of the ``AdamW`` optimizer
-
-        self.device = torch.device(
-                    "cuda" if torch.cuda.is_available() else
-                    "mps" if torch.backends.mps.is_available() else
-                    "cpu")
-
-        n_actions = 4
-        n_observations = self.height * self.width
-
-        self.policy_net = DQN(n_observations, n_actions).to(self.device)
-        self.target_net = DQN(n_observations, n_actions).to(self.device)
-        self.target_net.load_state_dict(self.policy_net.state_dict())
-
-        self.optimizer = optim.AdamW(self.policy_net.parameters(), lr=self.LR, amsgrad=True)
-        self.memory = ReplayMemory(10000)
-
-        self.steps_done = 0
-
     def pick_action(self):
         sample = rn.random()
         eps_threshold = self.EPS_END + (self.EPS_START - self.EPS_END) * \
@@ -223,7 +195,7 @@ class DLGrid(Grid):
 
     def reset(self):
         print(self.score)
-        print(self.memory)
+        # print(self.memory)
         self.score = 0
         self.board = self.initial_setup
         self.finished = False
