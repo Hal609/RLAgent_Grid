8fe4fc8e8fa2e502115f2bfccc719890fc913869
Hal609 <hal@kolb.co.uk>
2024-11-08 08:31:05 +0000

    Add epsilon to visualisation

--- board_cell_classes.py
+++ board_cell_classes.py
@@ -62,10 +62,10 @@ class DLGrid():
         self.trap_ratio = trap_ratio
         self.num_traps = 0
         self.action_map = {
-            0 : torch.tensor((0, 1)), 
-            1 : torch.tensor((0, -1)), 
-            2 : torch.tensor((1, 0)), 
-            3 : torch.tensor((-1, 0))}
+            0 : torch.tensor((0, 1), device=self.device), 
+            1 : torch.tensor((0, -1), device=self.device), 
+            2 : torch.tensor((1, 0), device=self.device), 
+            3 : torch.tensor((-1, 0), device=self.device)}
         
         self.colour_dict = {"treat": "ba0d8e66", "trap": "110c0baa", "agent": "db2046"}
         self.board_val_dict = {"000000ff": 0, "ffffffff": 1, "ba0d8e66": 2, "110c0baa": 3, "db2046": 4}
@@ -74,17 +74,19 @@ class DLGrid():
         self.state = self.board_to_tensor(self.board)
         self.initial_setup = deepcopy(self.board)
 
-        self.BATCH_SIZE = 128  # BATCH_SIZE is the number of transitions sampled from the replay buffer
-        self.GAMMA = 0.99  # GAMMA is the discount factor
+        self.BATCH_SIZE = 64  # BATCH_SIZE is the number of transitions sampled from the replay buffer
+        self.GAMMA = 0.95  # GAMMA is the discount factor
         self.EPS_START = 0.9  # EPS_START is the starting value of epsilon
+        self.EPS_CURRENT = self.EPS_START
         self.EPS_END = 0.05  # EPS_END is the final value of epsilon
         self.EPS_DECAY = 1000  # EPS_DECAY controls the rate of exponential decay of epsilon, higher means a slower decay
-        self.TAU = 0.005  # TAU is the update rate of the target network
-        self.LR = 1e-4  # LR is the learning rate of the ``AdamW`` optimizer
+        self.TAU = 0.010  # TAU is the update rate of the target network
+        self.LR = 2e-4  # LR is the learning rate of the ``AdamW`` optimizer
 
 
         n_actions = 4
-        n_observations = self.height * self.width
+        # n_observations = self.height * self.width
+        n_observations = 2
 
         self.policy_net = DQN(n_observations, n_actions).to(self.device)
         self.target_net = DQN(n_observations, n_actions).to(self.device)
@@ -120,66 +122,37 @@ class DLGrid():
                 self.board[y][x] = self.colour_dict["trap"]
                 traps += 1
 
-        self.agent_pos = torch.tensor((self.height//2, self.width//2))
-        self.board[self.agent_pos[0]][self.agent_pos[1]] = self.colour_dict["agent"]
-        print(self.board)
+        self.agent_pos = torch.tensor((self.height//2, self.width//2), dtype=torch.float32, device=self.device)
+        self.board[int(self.agent_pos[0])][int(self.agent_pos[1])] = self.colour_dict["agent"]
 
     def init_board(self):
-       self.agent_pos = torch.tensor((self.height//2, self.width//2))
-       self.board = np.array([['ffffffff', 'ffffffff', 'ffffffff', 'ffffffff', 'ffffffff', 'ffffffff',
-  'ffffffff', 'ffffffff', 'ffffffff', 'ffffffff', 'ffffffff', 'ffffffff',
-  'ffffffff', 'ffffffff', 'ffffffff', 'ffffffff', 'ffffffff', 'ffffffff'],
- ['ffffffff', '110c0baa', '110c0baa', '000000ff', 'ba0d8e66', 'ba0d8e66',
-  'ba0d8e66', '000000ff', 'ba0d8e66', 'ba0d8e66', '000000ff', 'ba0d8e66',
-  '000000ff', '000000ff', 'ba0d8e66', '110c0baa', '110c0baa', 'ffffffff'],
- ['ffffffff', 'ba0d8e66', '000000ff', '000000ff', '000000ff', 'ba0d8e66',
-  '000000ff', 'ba0d8e66', '110c0baa', '110c0baa', '000000ff', 'ba0d8e66',
-  '110c0baa', 'ba0d8e66', 'ba0d8e66', '110c0baa', 'ba0d8e66', 'ffffffff'],
- ['ffffffff', 'ba0d8e66', 'ba0d8e66', 'ba0d8e66', 'ba0d8e66', '000000ff',
-  'ba0d8e66', 'ba0d8e66', 'ba0d8e66', '110c0baa', 'ba0d8e66', '000000ff',
-  '000000ff', '110c0baa', '000000ff', 'ba0d8e66', 'ba0d8e66', 'ffffffff'],
- ['ffffffff', '000000ff', '000000ff', 'ba0d8e66', 'ba0d8e66', 'ba0d8e66',
-  'ba0d8e66', '000000ff', '000000ff', 'ba0d8e66', '110c0baa', '110c0baa',
-  'ba0d8e66', 'ba0d8e66', 'ba0d8e66', '000000ff', 'ba0d8e66', 'ffffffff'],
- ['ffffffff', 'ba0d8e66', 'ba0d8e66', '000000ff', '000000ff', '000000ff',
-  'ba0d8e66', '000000ff', 'ba0d8e66', '000000ff', 'ba0d8e66', 'ba0d8e66',
-  '000000ff', 'ba0d8e66', 'ba0d8e66', '000000ff', 'ba0d8e66', 'ffffffff'],
- ['ffffffff', '000000ff', 'ba0d8e66', '000000ff', 'ba0d8e66', '000000ff',
-  '110c0baa', 'ba0d8e66', '000000ff', 'ba0d8e66', 'ba0d8e66', '000000ff',
-  'ba0d8e66', '110c0baa', 'ba0d8e66', '000000ff', 'ba0d8e66', 'ffffffff'],
- ['ffffffff', '110c0baa', 'ba0d8e66', '000000ff', '000000ff', 'ba0d8e66',
-  'ba0d8e66', 'ba0d8e66', '000000ff', 'ba0d8e66', '000000ff', 'ba0d8e66',
-  '000000ff', '000000ff', 'ba0d8e66', 'ba0d8e66', '000000ff', 'ffffffff'],
- ['ffffffff', '000000ff', 'ba0d8e66', 'ba0d8e66', 'ba0d8e66', 'ba0d8e66',
-  'ba0d8e66', 'ba0d8e66', '110c0baa', '000000ff', '000000ff', 'ba0d8e66',
-  'ba0d8e66', 'ba0d8e66', '000000ff', '000000ff', '110c0baa', 'ffffffff'],
- ['ffffffff', 'ba0d8e66', '000000ff', 'ba0d8e66', 'ba0d8e66', '000000ff',
-  'ba0d8e66', '000000ff', 'ba0d8e66', 'db2046'  , 'ba0d8e66', 'ba0d8e66',
-  'ba0d8e66', 'ba0d8e66', '000000ff', 'ba0d8e66', 'ba0d8e66', 'ffffffff'],
- ['ffffffff', 'ba0d8e66', 'ba0d8e66', '110c0baa', 'ba0d8e66', '000000ff',
-  'ba0d8e66', '000000ff', '000000ff', '000000ff', 'ba0d8e66', 'ba0d8e66',
-  '000000ff', '110c0baa', 'ba0d8e66', 'ba0d8e66', '000000ff', 'ffffffff'],
- ['ffffffff', '000000ff', 'ba0d8e66', 'ba0d8e66', '110c0baa', 'ba0d8e66',
-  'ba0d8e66', 'ba0d8e66', 'ba0d8e66', 'ba0d8e66', '000000ff', 'ba0d8e66',
-  'ba0d8e66', 'ba0d8e66', 'ba0d8e66', '000000ff', 'ba0d8e66', 'ffffffff'],
- ['ffffffff', 'ba0d8e66', 'ba0d8e66', '000000ff', '000000ff', 'ba0d8e66',
-  'ba0d8e66', 'ba0d8e66', 'ba0d8e66', 'ba0d8e66', '110c0baa', '000000ff',
-  '000000ff', '000000ff', '110c0baa', 'ba0d8e66', '000000ff', 'ffffffff'],
- ['ffffffff', '000000ff', '000000ff', '110c0baa', 'ba0d8e66', 'ba0d8e66',
-  'ba0d8e66', '000000ff', '000000ff', '000000ff', 'ba0d8e66', 'ba0d8e66',
-  'ba0d8e66', '000000ff', 'ba0d8e66', '000000ff', 'ba0d8e66', 'ffffffff'],
- ['ffffffff', 'ba0d8e66', 'ba0d8e66', 'ba0d8e66', '000000ff', '000000ff',
-  '000000ff', '000000ff', 'ba0d8e66', 'ba0d8e66', 'ba0d8e66', '000000ff',
-  'ba0d8e66', 'ba0d8e66', 'ba0d8e66', '110c0baa', 'ba0d8e66', 'ffffffff'],
- ['ffffffff', '000000ff', 'ba0d8e66', 'ba0d8e66', '000000ff', '110c0baa',
-  '110c0baa', '000000ff', 'ba0d8e66', '000000ff', '000000ff', '000000ff',
-  '000000ff', '000000ff', 'ba0d8e66', '000000ff', '000000ff', 'ffffffff'],
- ['ffffffff', '000000ff', '110c0baa', 'ba0d8e66', '110c0baa', 'ba0d8e66',
-  'ba0d8e66', 'ba0d8e66', 'ba0d8e66', 'ba0d8e66', '110c0baa', 'ba0d8e66',
-  '000000ff', '000000ff', 'ba0d8e66', '000000ff', '000000ff', 'ffffffff'],
- ['ffffffff', 'ffffffff', 'ffffffff', 'ffffffff', 'ffffffff', 'ffffffff',
-  'ffffffff', 'ffffffff', 'ffffffff', 'ffffffff', 'ffffffff', 'ffffffff',
-  'ffffffff', 'ffffffff', 'ffffffff', 'ffffffff', 'ffffffff', 'ffffffff']])
+        self.board = np.zeros(self.size, dtype='U8')
+        for y, row in enumerate(self.board):
+            for x in range(len(row)):
+                if y == 0 or y == len(self.board) - 1 or x == 0 or x == len(row) - 1:
+                    self.board[y][x] = "ffffffff"
+                else:
+                    self.board[y][x] = "000000ff"
+        treats = 0
+        x, y = 0, 0
+        num_treats = self.height * self.width * self.treat_ratio
+        while treats < num_treats:
+            x = int(1 + ((x + 1.5) % (self.width - 2)))
+            y = int(1 + ((y + 2) % (self.height - 2)))
+            if self.board[y][x] != 1:
+                self.board[y][x] = self.colour_dict["treat"]
+                treats += 1
+        traps = 0
+        self.num_traps = (self.height * self.width - num_treats) * self.trap_ratio
+        while traps < self.num_traps:
+            x = int(1 + ((x + 2) % (self.width - 2)))
+            y = int(1 + ((y + 3) % (self.height - 2)))
+            if self.board[y][x] != 1:
+                self.board[y][x] = self.colour_dict["trap"]
+                traps += 1
+
+        self.agent_pos = torch.tensor((self.height//2, self.width//2), dtype=torch.float32, device=self.device)
+        self.board[int(self.agent_pos[0])][int(self.agent_pos[1])] = self.colour_dict["agent"]
     
     def is_inbounds(self, pos):
         if 0 in pos or self.width-1 <= pos[1] or self.height-1 <= pos[0]:
@@ -188,20 +161,20 @@ class DLGrid():
     
     def calc_reward(self, choice):
         new_pos = self.agent_pos + self.action_map[int(choice)]
-        next_val = self.board[new_pos[0]][new_pos[1]]
+        next_val = self.board[int(new_pos[0])][int(new_pos[1])]
         if next_val == self.colour_dict["treat"]:
-            return torch.tensor([5.0], device=self.device)
+            return 5.0
         elif next_val == self.colour_dict["trap"]:
-            return torch.tensor([-10.0], device=self.device)
-        return torch.tensor([-1.0], device=self.device)
+            return -10.0
+        return -1.0
     
     def next_state(self, choice):
         next_pos = self.agent_pos + self.action_map[int(choice)]
         if not self.is_inbounds(next_pos): return self.board
         next = deepcopy(self.board)
-        next[self.agent_pos[0]][self.agent_pos[1]] = "000000ff"
+        next[int(self.agent_pos[0])][int(self.agent_pos[1])] = "000000ff"
         self.agent_pos = next_pos
-        next[next_pos[0]][next_pos[1]] = self.colour_dict["agent"]
+        next[int(next_pos[0])][int(next_pos[1])] = self.colour_dict["agent"]
 
         return next
 
@@ -209,24 +182,26 @@ class DLGrid():
         tensor_board = torch.zeros((self.board.shape)).to(self.device)
         for y, row in enumerate(self.board):
             for x, cell in enumerate(row):
-                tensor_board[y][x] = self.board_val_dict[cell.lower()]
+                tensor_board[y][x] = torch.tensor(self.board_val_dict[cell.lower()], device=self.device)
         return tensor_board.flatten().unsqueeze(0)
         
     def is_done(self):
+        if self.score < -50: return True
         if int(torch.count_nonzero(self.board_to_tensor(self.board))) == self.num_traps + 1: return True
-        if self.initial_setup[self.agent_pos[0]][self.agent_pos[1]] == self.colour_dict["trap"]: return True
+        if self.initial_setup[int(self.agent_pos[0])][int(self.agent_pos[1])] == self.colour_dict["trap"]: return True
         return False
     
     def pick_action(self):
         sample = rn.random()
         eps_threshold = self.EPS_END + (self.EPS_START - self.EPS_END) * \
             math.exp(-1. * self.steps_done / self.EPS_DECAY)
-        # print("eps", eps_threshold)
+        self.EPS_CURRENT = eps_threshold
         self.steps_done += 1
         if sample > eps_threshold:
             # Take deliberate action
             with torch.no_grad():
-                return torch.argmax(self.policy_net(self.state)).unsqueeze(0).unsqueeze(0)
+                return torch.argmax(self.policy_net(self.agent_pos.unsqueeze(0))).unsqueeze(0).unsqueeze(0)
+                # return torch.argmax(self.policy_net(self.state)).unsqueeze(0).unsqueeze(0)
         else:
             # Take random action
             return torch.tensor([[rn.randint(0, 3)]], device=self.device)
@@ -249,6 +224,7 @@ class DLGrid():
         state_batch = torch.cat(batch.state)
         action_batch = torch.cat(batch.action)
         reward_batch = torch.cat(batch.reward)
+        # print("state batch shape", state_batch.shape)
 
         # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
         # columns of actions taken. These are the actions which would've been taken
@@ -270,7 +246,6 @@ class DLGrid():
         # Compute Huber loss
         criterion = nn.SmoothL1Loss()
         loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
-        # print("loss", float(loss))
 
         # Optimize the model
         self.optimizer.zero_grad()
@@ -282,8 +257,10 @@ class DLGrid():
     def step(self, current_grid):
         if self.finished: self.reset()
 
+        start_pos = self.agent_pos.unsqueeze(0)
         action = self.pick_action()
         reward = self.calc_reward(action)
+        reward = torch.tensor([reward], device=self.device)
         self.score += reward
         next_board = self.next_state(action)
         self.finished = self.is_done()
@@ -293,9 +270,9 @@ class DLGrid():
         else:
             next_state = self.board_to_tensor(next_board)
 
-        self.memory.push(self.board_to_tensor(self.board), action, self.board_to_tensor(next_state), reward)
-        self.board = next_state
         self.state = self.board_to_tensor(self.board)
+        self.memory.push(start_pos, action, self.agent_pos.unsqueeze(0), reward)
+        self.board = next_board
 
         # Perform one step of the optimization (on the policy network)
         self.optimize_model()
@@ -308,13 +285,14 @@ class DLGrid():
             target_net_state_dict[key] = policy_net_state_dict[key]*self.TAU + target_net_state_dict[key]*(1-self.TAU)
         self.target_net.load_state_dict(target_net_state_dict)
 
-        return next_state
+        return next_board
 
     def run_n_episodes(self, n, tick_rate = 0, vis=False):
         for i in range(n):
             if vis:
                 run_grid(self.board, update_func=self.step, tick_rate=tick_rate)
             else:
+                self.reset()
                 while not self.finished:
                     self.step(None)
 
